ef9ce0712936e1fd8632fa7ee0d10230ed85589e
Abdur Rahman Shafi <111105311+shafibr21@users.noreply.github.com>
2024-03-03 15:06:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/* ====================== typing animation =========================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Graphic Designer","App Developer","SEO Expert"],
+    typeSpeed: 100,
+    BackSpeed: 80,
+    loop: true
+})
+
+    // Function to display page location
+    function displayPageLocation() {
+        var pageLocation = window.location.href;
+        document.getElementById('page-location').textContent = "Page Location: " + pageLocation;
+    }
+
+    // Function to display last modification date
+    function displayLastModifiedDate() {
+        var lastModified = document.lastModified;
+        document.getElementById('last-modification-date').textContent = "Last Modified: " + lastModified;
+    }
+
+    // Call the functions when the page loads
+    window.onload = function() {
+        displayPageLocation();
+        displayLastModifiedDate();
+    };
